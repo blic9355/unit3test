56c18da4aa88762a28f8999dbb714db49623bdb7
Brian Licea <brianlicea@Brians-MacBook-Air.local>
2017-10-06 12:10:21 -0600

    finished the app

---
+++ unit3testpart2/model/Movie.swift
@@ -0,0 +1,9 @@
+//
+//  Movie.swift
+//  unit3testpart2
+//
+//  Created by Brian Licea on 10/6/17.
+//  Copyright © 2017 Brian Licea. All rights reserved.
+//
+
+import Foundation
---
+++ unit3testpart2/view/MovieTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieTableViewCell.swift
+//  unit3testpart2
+//
+//  Created by Brian Licea on 10/6/17.
+//  Copyright © 2017 Brian Licea. All rights reserved.
+//
+
+import UIKit
+
+class MovieTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
